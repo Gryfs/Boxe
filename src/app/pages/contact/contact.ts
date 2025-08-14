dc790e34b4246224cc2ff3fa653333dc4b5b4ab4
OceLithia <ocebrs@gmail.com>
2025-08-14 17:49:43 +0200

    maj formulaire

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule,],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrls: ['./contact.css']
 })
 export class Contact {
+  contactForm: FormGroup = new FormGroup({
+    nom: new FormControl('', Validators.required),
+    prenom: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required)
+  });
 
+  submitted: boolean = false;
+  formSent = false;
+  sending: boolean = false;
+
+  onSubmit() {
+    // gestion de la soumission du formulaire ici
+    this.submitted = true;
+    if (this.contactForm.valid) {
+      this.sending = true;
+      setTimeout(() => {
+        console.log(this.contactForm.value);
+        this.formSent = true;
+        this.contactForm.reset();
+        this.submitted = false;
+        this.sending = false;
+      }, 1500);
+    }
+  }
 }
